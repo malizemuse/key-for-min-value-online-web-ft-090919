b230d2a3ffe5d40d6efe1cb54d89e85fe1447a30
gregarious-number-3828 <muse.malize@gmail.com>
2019-09-15 23:29:35 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,14 +2,23 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  
-  name_hash. do |key, value|
+ 
+ # If hash is empty, return nil
+  if name_hash = {}
+    return nil
     
-    puts min_val = value
-    
-  end
-  
-  
+  else
+    # Set min_value to 
+    min_value = name_hash.first[1]
+    min_key = name_hash.first[0]
+    name_hash.each do |key, value|
+      if value < min_value
+        min_value = value
+        min_key = key
+      end
+    end
+    min_key
+  end 
   
 end
 
