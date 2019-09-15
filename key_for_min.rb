ce5215a0541591cfd960e6b36f0f7e6ac981d077
gregarious-number-3828 <muse.malize@gmail.com>
2019-09-15 23:16:11 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,28 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  
+  name_hash.collect do |key, value|
+    
+    min_key = key
+    
+    value
+    
+    #if value < min_key[key]
+    #  min_key = key
+    #end
+    
+  end
+  
+  #min_key
+  
 end
+
+
+# TEST CODE 
+
+ikea = {:chair => 25, :table => 85, :mattress => 450}
+puts key_for_min_value(ikea)
+
+veggies = {"apple" => -45, "banana" => -44.5, "carrot" => -44.9}
+puts key_for_min_value(veggies)
