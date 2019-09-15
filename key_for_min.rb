b44b26bf5785821323181a8298f8ae6f8667c894
gregarious-number-3828 <muse.malize@gmail.com>
2019-09-15 23:32:26 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -12,6 +12,7 @@ def key_for_min_value(name_hash)
     min_value = name_hash.first[1]
     min_key = name_hash.first[0]
     
+    # Change min key and value if 
     name_hash.each do |key, value|
       if value < min_value
         min_value = value
